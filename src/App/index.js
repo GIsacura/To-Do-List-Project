070a7ed2489242aab2075ac50384924922d6491b
Gustavo Isacura <i.tavo1512@gmail.com>
2021-09-27 22:36:15 -0400

    Clase 4: composicion de componentes y colocacion de estado

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,60 @@
 import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
+// import { TodoProvider } from '../TodoContext';
+// import { TodoSearch } from '../TodoSearch';
+// import { AppUI } from './AppUI';
 
+function App(){
 
-function App() {
-  return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+  const [state, setState] = React.state('Estado compartido')
+
+  return(
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter/>
+        <TodoSearch/>
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state}/>
+      </TodoList>
+    </React.Fragment>
   );
 }
+
+function TodoHeader({children}){
+  return(
+    <header>
+      {children}
+    </header>
+  )
+}
+
+function TodoList({children}){
+  return (
+    <section className="TodoList-container">
+      {children}
+    </section>
+  )
+}
+
+function TodoCounter(){
+  return <p>TodoCounter</p>
+}
+
+function TodoSearch(){
+  return <p>TodoSearch</p>
+}
+
+function TodoItem({state}){
+  <p>TodoItem:{state}</p>
+}
+
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
